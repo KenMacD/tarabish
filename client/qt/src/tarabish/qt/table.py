d9fbfc5b85d64609b7e181edc7b92e28f79ff4d1
Kenny MacDermid <kenny.macdermid@gmail.com>
2011-09-04 14:17:02 -0300

    Add the start of a card box to display your cards.

--- client/qt/src/tarabish/qt/table.py
+++ client/qt/src/tarabish/qt/table.py
@@ -1,3 +1,7 @@
+from tarabish.thrift.constants import (CLUBS, SPADES, HEARTS, DIAMONDS)
+from tarabish.thrift.constants import (JACK, QUEEN, KING, ACE)
+from tarabish.thrift.ttypes import (Card)
+from PySide.QtCore import QSize
 from PySide.QtGui import *
 
 class ChatWidget(QWidget):
@@ -23,6 +27,62 @@ class ChatWidget(QWidget):
         self.messages.append(self.message_box.text())
         self.message_box.clear()
 
+class CardWidget(QWidget):
+    suit = {CLUBS: "C", SPADES: "S", HEARTS: "H", DIAMONDS: "D"}
+    value = {JACK: "J", QUEEN: "Q", KING: "K", ACE: "A"}
+
+    def __init__(self, pyCard, parent=None):
+        super(CardWidget, self).__init__(parent)
+
+        self.card = pyCard
+        frame = QFrame(self)
+        frame.setFrameStyle(QFrame.Box);
+        frame.setFixedSize(70, 90)
+
+        suit = CardWidget.suit[pyCard.suit]
+        if pyCard.value > 10:
+            value = CardWidget.value[pyCard.value]
+        else:
+            value = str(pyCard.value)
+
+        value = "%s %s" %(value, suit)
+
+        valueLabel = QLabel(value, self)
+        valueLabel.move(1, 0)
+
+    def sizeHint(self):
+        return self.minimumSizeHint()
+
+    def minimumSizeHint(self):
+        return QSize(70, 90)
+
+
+class CardBoxWidget(QWidget):
+    def __init__(self, cards=None, trump=None, parent=None):
+        super(CardBoxWidget, self).__init__(parent)
+
+        self.cardLayout = QHBoxLayout()
+        self.resize(200, 100)
+
+        if not cards:
+            self.cards = []
+        else:
+            self.cards = cards
+            self.add_cards(cards)
+
+        self.trump = trump
+        self.cardLayout.addStretch()
+
+        self.setLayout(self.cardLayout)
+
+    def add_cards(self, cards):
+        for card in cards:
+            item = CardWidget(card)
+            self.cardLayout.addWidget(item)
+
+        self.cards = self.cards.append(cards)
+
+
 class Table(QDialog):
     def __init__(self, tableId, eventSignal, logger, parent=None):
         super(Table, self).__init__(parent)
@@ -37,6 +97,9 @@ class Table(QDialog):
         hbox.addWidget(ChatWidget())
 
         vbox = QVBoxLayout()
+
+        cards = [Card(10, CLUBS), Card(ACE, HEARTS)]
+        vbox.addWidget(CardBoxWidget(cards))
         vbox.addLayout(hbox)
 
         self.setLayout(vbox)
