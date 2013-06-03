6f247eac8160551d07042da052a636c72bc131be
Kenny MacDermid <kenny.macdermid@gmail.com>
2013-06-03 19:34:21 -0300

    Test with pure websockets.
    Remove sockjs for now, it can be added back in after changes are
    made to the API.

--- server/src/main.erl
+++ server/src/main.erl
@@ -7,11 +7,11 @@ start() ->
   mnesia:start(),
   install_tables([node()], []), % Install all in RAM for now
   crypto:start(),
-  tarabish_server:start(),
-  tarabish_server:create_table(),
-  tarabish_server:create_table(),
-  tarabish_server:create_table(),
-  thrift_cmd:start(),
+%  tarabish_server:start(),
+%  tarabish_server:create_table(),
+%  tarabish_server:create_table(),
+%  tarabish_server:create_table(),
+%  thrift_cmd:start(),
   web:start().
 
 install_tables(RamNodes, DiskNodes)
--- server/src/web.erl
+++ server/src/web.erl
@@ -1,62 +1,64 @@
 -module(web).
 
--include("tarabish_thrift.hrl").
--include("tarabish_constants.hrl").
-
 -export([start/0]).
+-behaviour(cowboy_websocket_handler).
 
--export([init/3, handle/2, terminate/2]).
-
--record(state, {processor, transport}).
+-export([init/3]).
+-export([websocket_init/3]).
+-export([websocket_handle/3]).
+-export([websocket_info/3]).
+-export([websocket_terminate/3]).
 
 start() ->
-  Port = 42747,
-  application:start(sockjs),
-  application:start(cowboy),
+  Port = 42745,
+  ok = application:start(ranch),
+  ok = application:start(cowboy),
 
-  SockjsState = sockjs_handler:init_state(
-    <<"/test">>, fun service_test/3, #state{}, []),
-  VhostRoutes = [
-    {[<<"test">>, '...'], sockjs_cowboy_handler, SockjsState},
-    {[<<"static">>, '...'], cowboy_http_static, [{directory, "docroot"}]},
-    {'_', ?MODULE, []}],
-  Routes = [{'_', VhostRoutes}], % any vhost
+  % TODO: setup as application as use priv_dir
+  {ok, Cwd} = file:get_cwd(),
+  Path = filename:join([Cwd, "docroot"]),
+  Dispatch = cowboy_router:compile([
+      {'_', [
+        {"/", cowboy_static, [
+          {directory, Path},
+%          {file, "html_ws_client.html"},
+          {file, "index.html"},
+          {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+        ]},
+        {"/websocket", ?MODULE, []},
+        {"/static/[...]", cowboy_static, [
+          {directory, filename:join([Path, "static"])},
+          {mimetypes, {fun mimetypes:path_to_mimes/2, default}}
+        ]}
+      ]}]),
+
+  {ok, _} = cowboy:start_http(http, 100, [{port, Port}],
+    [{env, [{dispatch, Dispatch}]}]),
 
   io:format(" [*] Running at http://localhost:~p~n", [Port]),
-
-  cowboy:start_listener(http, 100,
-    cowboy_tcp_transport, [{port,     Port}],
-    cowboy_http_protocol, [{dispatch, Routes}]),
 
   receive
     _ -> ok
   end.
 
-init({_Any, http}, Req, []) ->
-  {ok, Req, []}.
+% Test of example code.
+init({tcp, http}, _Req, _Opts) ->
+  {upgrade, protocol, cowboy_websocket}.
 
-handle(Req, State) ->
-  {ok, Data} = file:read_file("./docroot/index.html"),
-  {ok, Req1} = cowboy_http_req:reply(200, [{<<"Content-Type">>, "text/html"}],
-                                     Data, Req),
-  {ok, Req1, State}.
+websocket_init(_TransportName, Req, _Opts) ->
+  erlang:start_timer(1000, self(), <<"Hello!">>),
+  {ok, Req, undefined_state}.
 
-terminate(_Req, _State) ->
+websocket_handle({text, Msg}, Req, State) ->
+  {reply, {text, << "You said: ", Msg/binary >>}, Req, State};
+websocket_handle(_Data, Req, State) ->
+  {ok, Req, State}.
+
+websocket_info({timeout, _Ref, Msg}, Req, State) ->
+  erlang:start_timer(5000, self(), <<".">>),
+  {reply, {text, Msg}, Req, State};
+websocket_info(_Info, Req, State) ->
+  {ok, Req, State}.
+
+websocket_terminate(_Reason, _Req, _State) ->
   ok.
-
-service_test(Conn, init, State) ->
-%   Conn:send(sockjs_json:encode({struct:q
-  {ok, #state{}};
-
-service_test(_Conn, {recv, Data}, #state{transport = Transport} = State) ->
-  case sockjs_json:decode(Data) of
-    {ok, V} -> handle_json(V);
-    {error, E} -> ok
-  end,
-  {ok, State};
-
-service_test(_Conn, closed, State) ->
-  {ok, state}.
-
-handle_json(Object) ->
-  io:format(" [*] Received Data ~p~n", [Object]).
