d60f59345e6cbee9f0711a53849966189de96304
Kenny MacDermid <kenny.macdermid@gmail.com>
2013-08-16 20:00:15 -0300

    Bots play cards now.
    They just try to play all their cards each time, but it's enough.

--- client/py/bot.py
+++ client/py/bot.py
@@ -8,12 +8,23 @@ import random
 
 import websocket
 
+myseat = -1
+cards = []
+
+def tome(event):
+    if 'seat' in event and event['seat'] == myseat:
+        return True
+    return False
+
 def wssend(ws, method, **kwargs):
     print "Sending method %s" %(method,)
-    message = {"method": method}
-    message.update(kwargs)
-    jmsg = json.dumps(message)
-    ws.send(jmsg)
+    try:
+        message = {"method": method}
+        message.update(kwargs)
+        jmsg = json.dumps(message)
+        ws.send(jmsg)
+    except Exception as e:
+        print "Error sending: " + str(e)
 
 def on_open(ws):
     print "On open called"
@@ -43,131 +54,27 @@ def on_message(ws, message):
                 return
             for seat in table['seats']:
                 if seat['isOpen']:
+                    global myseat
+                    myseat = seat["num"]
                     wssend(ws, "sit", table_id=tid, seat=seat["num"])
                     return
+    elif mtype == "deal":
+        for card in event['dealt']:
+            suit = card['suit']
+            value = card['value']
+            cards.append((value, suit))
+    elif mtype == "ask_trump" and tome(event):
+        wssend(ws, "call_trump", table_id=1, suit=1)
+    elif mtype == "game_cancel":
+        global cards
+        cards = []
+    elif mtype == "ask_card" and tome(event):
+        for (val, suit) in cards:
+            wssend(ws, "play_card", table_id=1, card = {"value":val, "suit":suit})
 
 
 ws = websocket.WebSocketApp("ws://127.0.0.1:42745/websocket",
         on_message = on_message)
 ws.on_open = on_open
 ws.run_forever()
-sys.exit(1)
-
-################################################################################
-# Old version:
-################################################################################
-
-print "Get Version:" + str(client.getVersion())
-
-name = ''.join(random.choice(string.ascii_letters) for x in range(5))
-
-print "Creating account %s "%(name,),
-
-try:
-    print str(client.createAccount(name, "%s@invalid"%(name,), "password")),
-except InvalidOperation as invalid:
-    print "Didn't work, perhaps the account exists"
-
-print "Login"
-cookie = client.login(name, "password")
-
-print "Checking Table list - ",
-tables = client.getTables(cookie)
-print str(tables)
-
-tablenum = 0
-def join_first_seat(tables):
-    for table in tables:
-        seat_num = 0
-        for seat in table.seats:
-            if seat.isOpen:
-                print "Found open seat"
-                client.sit(cookie, table.tableId, seat_num)
-                if seat_num == 3:
-                    print "Starting the game"
-                    global tablenum
-                    tablenum = table.tableId
-                    client.startGame(cookie, table.tableId)
-                return (table.tableId, seat_num)
-            seat_num += 1
-
-print "Joining table"
-(tableid, seatnum) = join_first_seat(tables)
-
-
-def join_event(cookie):
-    print "Joining Event Stream, ",
-    t2 = TSocket.TSocket('localhost', 42745)
-    t2 = TTransport.TBufferedTransport(t2)
-    p2 = TBinaryProtocol.TBinaryProtocol(t2)
-    t2.open()
-    event_client = Tarabish.Client(p2)
-    return event_client
-
-game = 0
-ec = join_event(cookie)
-cards = []
-trick = 0
-event_last = 0
-#count = 0
-while True:
-    print "Getting Events..."
-    events = ec.getEventsTimeout(cookie, event_last, 300000)
-    if events:
-        event_last = events[-1].number
-    for event in events:
-        print_event(event, seatnum)
-#        if event.type == EventType.HAND_DONE:
-#            count = count + 1
-#            print "BELLA hand count: " + str(count)
-        if event.type == EventType.DEAL:
-            cards += event.dealt
-            trick = 0
-        if event.type == EventType.ASK_TRUMP and event.seat == seatnum:
-            try:
-                client.callTrump(cookie, tableid, PASS)
-            except InvalidOperation, e:
-                # Forced
-                client.callTrump(cookie, tableid, SPADES)
-        if event.type == EventType.PLAY_CARD and event.seat == seatnum:
-             cards.remove(event.card)
-        if event.type == EventType.ASK_CARD and event.seat == seatnum:
-            if trick == 0:
-                try:
-                    client.callRun(cookie, tableid)
-                    print "Called Run!"
-                except InvalidOperation, e:
-                    pass # expected
-            if trick == 1:
-                try:
-                    client.showRun(cookie, tableid)
-                    print "Showed Run!"
-                except InvalidOperation, e:
-                    pass # expected
-            played = 0
-            # try playing each card a bells first:
-            try:
-                client.playBella(cookie, tableid)
-                print "Played Bella!"
-                played = 1
-            except InvalidOperation, e:
-                for card in cards[:]:
-                    try:
-                        client.playCard(cookie, tableid, card)
-                        played = 1
-                        break;
-                    except InvalidOperation, e:
-                        pass # expected
-
-            if not played:
-                print "!!! No valid cards in hand: " + str(cards)
-            trick = trick + 1
-        if event.type == EventType.GAME_DONE:
-            game = game + 1
-            if game < 5 and seatnum == 3:
-                client.startGame(cookie, tablenum)
-            elif game == 5:
-                sys.exit(1)
-
-
 sys.exit(1)
