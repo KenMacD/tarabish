8f6fb7222ff4ceb12bdef9aa1dd03efa3f5f892f
Kenny MacDermid <kenny.macdermid@gmail.com>
2013-11-22 21:23:52 -0400

    Biggensize and add green.

--- client/tarabishdart/web/the_table.dart
+++ client/tarabishdart/web/the_table.dart
@@ -8,13 +8,26 @@ class TheTable extends CanvasElement with Polymer, Observable{
 
   TheTable.created() : super.created() {
     print("TheTable Created");
+
     /* TODO: fill in */
   }
 
   void enteredView() {
     super.enteredView();
+    print("TheTable Entered View");
+    //width = window.innerWidth;
+    //height = window.innerHeight;
+
     _context = getContext("2d");
+
+    _context.fillStyle = "#27462c";
+    _context.fillRect(0, 0, window.innerWidth, window.innerHeight);
+
+    _context.fillStyle = "#000000";
     _context.fillText("TESTING", 100, 100);
+
+
   }
+
 
 }
