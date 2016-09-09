188cf6e19524c1aca9dd3fcb946703dfac4d5b0e
Kenny MacDermid <kenny.macdermid@gmail.com>
2016-09-08 22:57:21 -0300

    Add the start of a websocket protocol.
    The login message is sent to the server.

--- client/react/src/App.js
+++ client/react/src/App.js
@@ -3,12 +3,41 @@ import './App.css';
 import Table from './Table';
 
 class App extends Component {
+  constructor () {
+    super();
+    const ws = new WebSocket('ws://localhost:42745/websocket')
+    this.state = {
+      ws: ws
+    }
+    ws.onopen = () => console.log("OPENED")
+  };
+
+  login = (event) => {
+    let name = this.nameInput.value
+    if (!name) {
+      console.log("No name supplied");
+      return
+    }
+    console.log("Logging in as " + name);
+    let msg = {
+      method: "login",
+      name: name
+    }
+    this.state.ws.send(JSON.stringify(msg))
+  }
+
   render() {
     return (
       <div>
        <div className="App">
           <div className="App-header">
             <h2>Welcome to Tarabish Online</h2>
+          </div>
+          <div>
+            <input type="text" placeholder="Name"
+              ref={(ref) => this.nameInput = ref}
+            />
+            <input type="button" value="Login" onClick={this.login} />
           </div>
           <div><Table name="Test Table 1"/></div>
         </div>
