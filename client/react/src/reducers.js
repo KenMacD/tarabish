5ec305c02a4202789ceb371924894d68be90fb02
Kenny MacDermid <kenny.macdermid@gmail.com>
2016-10-26 22:26:17 -0300

    Add a tables reducer to the lobby table list.

--- client/react/src/reducers.js
+++ client/react/src/reducers.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux'
 import { WS_MSG } from './actions'
 
+
+// TODO: Should just one reducer accept WS_MSG
+// and repack the message and resend them?
+
 function auth(state = {isAuthenticated: false}, action) {
   switch (action.type) {
     case WS_MSG:
@@ -21,8 +25,31 @@ function auth(state = {isAuthenticated: false}, action) {
   return state;
 }
 
+function tables(state = {tableList: []}, action) {
+  switch (action.type) {
+    // get-tables, sit, stand...
+    case WS_MSG:
+      let msg = action.msg
+      switch (msg.type) {
+        case "tables":
+          return {
+            ...state,
+            tableList: msg.tables,
+          }
+        default:
+          break
+      }
+      break
+    default:
+      break
+  }
+  return state;
+}
+
+
 const tarabishApp = combineReducers({
-  auth
+  auth,
+  tables
 })
 
 export default tarabishApp
